234f553106a467a8b3c1046fdb44262dff273e26
KArthick707 <68534141+KArthick707@users.noreply.github.com>
2020-07-22 14:16:25 +0530

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+n = int(input("Enter n:"))
+sum = 0
+c = 1
+while c<=n:
+    print(c)
+    sum = sum + c 
+    c+=1
+    
+
+
+# In[2]:
+
+
+num = int(input("ENTER THE NUMBER:"))
+for i in range(2,num):
+    if num % i == 0:
+        print("not prime number")
+        break
+else:
+    print("it is a prime number")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
